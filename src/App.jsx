649d9fb77a7fc17b8da7339ea71b9a0d694ecdcf
Ranger-2327 <ak8709368190@gmail.com>
2024-10-23 17:26:18 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FeatureSection from './components/FeatureSection';
-import Workflow from './components/WorkFlow';
+import Workflow from './components/Workflow';
 import Pricing from './components/Pricing';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
